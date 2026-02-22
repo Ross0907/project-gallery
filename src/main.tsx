983193d4e9f7f40bd88ee2a94c786137ad11e2f9
Ross0907 <140931356+Ross0907@users.noreply.github.com>
2026-02-22 10:54:08 +0530

    adsadsasd

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-const params = new URLSearchParams(window.location.search);
-const redirect = params.get("redirect");
-
-if (redirect) {
-  window.history.replaceState(null, "", redirect);
-}
 createRoot(document.getElementById("root")!).render(<App />);
